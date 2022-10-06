e1a093105d9831fccab32e7c5162634c265bdd83
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-10-06 18:48:32 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, {useState,useEffect} from 'react'
+import Loading from './Loading';
 import './App.css';
-
+const url = "https://course-api.com/react-tabs-project";
 function App() {
+  const  [loading,setLoading] = useState(true)
+  const [getJobs,setGetJobs] = useState([])
+  const [value, setValue] = useState(0)
+  const fetchJobs = async()=>{
+    const res = await fetch(url)
+    const jobs = await res.json()
+    setGetJobs(jobs)
+    setLoading(false)
+  }
+  useEffect(()=>{
+    fetchJobs()
+  },[])
+  if(loading){
+    return <Loading/>
+  }
+    const { company, dates, duties, title } = getJobs[value];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className='title'>
+    <h2>experience</h2>
+    <div className='underLine'></div>
+      </div>
+      <div className='jobs-center'>
+        <div className='btn-container'>
+          {getJobs.map((job, index)=>{
+            return <button key={job.id} onClick={()=>setValue(index)} className={`job-btn ${index === value && 'active-btn'}`}>
+              {job.company}
+            </button>
+          })}
+        </div>
+        {/*job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty,index)=>{
+            return (
+              <div key={index} className='job-desc'> <span className='job-icon'> >> </span>
+              <p>{duty}</p>
+              </div>
+          )})}
+        </article>
+      </div>
+    </section>
   );
 }
 
 export default App;
+
---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <section className='section loading'>
+      <h2>Getting jobs for you...</h2>
+    </section>
+  );
+}
+
+export default Loading
